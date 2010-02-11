12e9e6a9b29a9411b28d780e6fadc2e24d8cb1a5
Matt Swanson <mdswanso@devel.ecn.purdue.edu>
2010-02-11 11:09:17 -0500

    ffffuuu

--- testbed/udp_server.py
+++ testbed/udp_server.py
@@ -1,9 +1,19 @@
 from twisted.internet.protocol import DatagramProtocol
 from twisted.internet import reactor
+from MySQLdb import *
+from getpass import *
 
 class SimpleUDPServer(DatagramProtocol):
     def startProtocol(self):
         print "Starting server..."
+        try:
+            pw = getpass()
+            db = connect(host="localhost",user="mdswanso",passwd=pw, \
+                                db="iphone_obd2",port=3306, connect_timeout=5)
+        except OperationalError:
+            return None
+        return db
+
 
     def datagramReceived(self, datagram, address):
         print "Received:" + repr(datagram)
