4e4c2cca716a7bc34b29dde41e78f8b3ba9143bc
Matt Swanson <swanson@purdue.edu>
2010-02-18 20:09:22 -0500

    adding updated socket client

--- testbed/socketclient.py
+++ testbed/socketclient.py
@@ -1,10 +1,11 @@
 from socket import *
 import sys
 
-host, port = ('localhost', 8005)
+host, port = ('128.46.96.170', 8005)
 
 server = socket(AF_INET, SOCK_DGRAM)
 server.connect((host,port))
+print 'starting'
 while 1:
     try:
         msg = sys.stdin.readline()
